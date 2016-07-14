2652d361f53c44202a37a27449b41b4f89ea525e
YaoShangTseng <yakushou730@gmail.com>
2016-07-14 16:37:32 +0800

    fix Facebook api login/logout (iHower version)

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,16 +5,15 @@ class ApiController < ActionController::Base
   before_action :authenticate_user_from_token!
 
   def authenticate_user_from_token!
-#byebug
+
     if params[:auth_token].present?
-#byebug
+
       user = User.find_by_authentication_token( params[:auth_token] )
-#byebug
+
       # Devise: 設定 current_user
       sign_in(user, store: false) if user
-
-    else
-      render :json => { :message => "authenticate user token fail" }, :status => 401
+    #else
+      #render :json => { :message => "authenticate user token fail" }, :status => 401
     end
   end
 
